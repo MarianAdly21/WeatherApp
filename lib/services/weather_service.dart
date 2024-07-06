ac95d546f27f70a9afa9fde4698e8f394eba6965
Marian Adly <marianadly2004@gmail.com>
2024-07-06 17:21:02 +0300

    create base url and handling exception

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -4,12 +4,17 @@ import 'package:weather_app/model/weather_model.dart';
 class WeatherService {
   final Dio dio;
   final String baseUrl = "http://api.weatherapi.com/v1";
+  final String apiKey = "8a31ff0257c843aeb30110243240407";
   WeatherService(this.dio);
 
-  Future<WeatherModel> getCurrentWeather({required String cityName}) async {
-    Response response = await dio.get(
-        '$baseUrl/forecast.json?key=8a31ff0257c843aeb30110243240407&q=$cityName&days=10');
-    WeatherModel weatherModel = WeatherModel.fromJson(response.data);
-    return weatherModel;
+  Future<WeatherModel?> getCurrentWeather({required String cityName}) async {
+    try {
+      Response response = await dio
+          .get('$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=10');
+      WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+      return weatherModel;
+    } catch (e) {
+      return null;
+    }
   }
 }
