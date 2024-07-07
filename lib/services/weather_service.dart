b16d17f7347db336845c756801715ccb32700624
Marian Adly <marianadly2004@gmail.com>
2024-07-07 16:58:30 +0300

    Dio Exception

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:dio/dio.dart';
 import 'package:weather_app/model/weather_model.dart';
 
@@ -7,14 +9,21 @@ class WeatherService {
   final String apiKey = "8a31ff0257c843aeb30110243240407";
   WeatherService(this.dio);
 
-  Future<WeatherModel?> getCurrentWeather({required String cityName}) async {
+  Future<WeatherModel> getCurrentWeather({required String cityName}) async {
     try {
       Response response = await dio
           .get('$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=10');
+
       WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+
       return weatherModel;
+    } on DioException catch (e) {
+      final String errorMessage = e.response?.data["error"]["message"] ??
+          "Oops there was an error, try later";
+      throw Exception(errorMessage);
     } catch (e) {
-      return null;
+      log(e.toString());
+      throw Exception("Oops there was an error, try later");
     }
   }
 }
