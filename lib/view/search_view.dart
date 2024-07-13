b6a632585cabddff24733d6045d2eb4a4753e904
Marian Adly <marianadly2004@gmail.com>
2024-07-13 22:48:21 +0300

    Trigger cubit

--- lib/view/search_view.dart
+++ lib/view/search_view.dart
@@ -2,6 +2,8 @@ import 'dart:developer';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_cubit.dart';
 import 'package:weather_app/model/weather_model.dart';
 import 'package:weather_app/services/weather_service.dart';
 
@@ -12,9 +14,9 @@ class SearchView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.white),
+        iconTheme: const IconThemeData(color: Colors.white),
         backgroundColor: Colors.blue,
-        title: Text(
+        title: const Text(
           "Search A City",
           style: TextStyle(
             color: Colors.white,
@@ -27,19 +29,22 @@ class SearchView extends StatelessWidget {
         child: Center(
           child: TextField(
             onSubmitted: (value) {
+              var getWeatherCubit = BlocProvider.of<GetWeatherCubit>(context);
+              getWeatherCubit.getWeather(cityName: value);
               Navigator.pop(context);
             },
             decoration: InputDecoration(
-              contentPadding: EdgeInsets.symmetric(
+              contentPadding: const EdgeInsets.symmetric(
                 vertical: 32,
                 horizontal: 16,
               ),
               hintText: "Enter City Name",
-              suffixIcon: Icon(Icons.search),
+              suffixIcon: const Icon(Icons.search),
               labelText: "Search",
               border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(color: Colors.green)),
+                borderRadius: BorderRadius.circular(10),
+                borderSide: const BorderSide(color: Colors.green),
+              ),
             ),
           ),
         ),
