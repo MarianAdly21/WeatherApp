b800a0ffdad071601ee4376ff1683cbcc216364b
Marian Adly <marianadly2004@gmail.com>
2024-07-13 19:32:32 +0300

    integrate cubit

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_cubit.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_states.dart';
 import 'package:weather_app/view/search_view.dart';
 import 'package:weather_app/view/weather_info_body.dart';
 import 'package:weather_app/widgets/no_weather_body.dart';
@@ -35,7 +38,16 @@ class HomeView extends StatelessWidget {
           ),
         ),
       ),
-      body: const NoWeatherBody(),
+      body:
+          BlocBuilder<GetWeatherCubit, WeatherState>(builder: (context, state) {
+        if (state is WeatherInitialState) {
+          return NoWeatherBody();
+        } else if (state is WeatherLoadedState) {
+          return WeatherInfoBody();
+        } else {
+          return Text('opps there was an error ');
+        }
+      }),
     );
   }
 }
